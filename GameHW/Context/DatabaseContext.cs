30c8da1d3e49c5ac810fb534524cf2331ea2fffc
Minna Angelova <minnaangelova@abv.bg>
2019-05-21 00:56:26 +0300

    Add Database Initializer

---
+++ GameHW/Context/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using GameHW.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GameHW.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("DefaultConnection") { }
+
+        public DbSet<Game> Games { get; set; }
+    }
+}
---
+++ GameHW/Context/DatabaseInitializer.cs
@@ -0,0 +1,57 @@
+﻿using GameHW.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace GameHW.Context
+{
+    public class DatabaseInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<DatabaseContext>
+    {
+        protected override void Seed(DatabaseContext context)
+        {
+
+
+            base.Seed(context);
+
+              var games = new List<Game>
+              {
+                new Game
+                {
+                    GameId = 1,
+                    GameTitle = "Mortal Combat 5"
+                },
+
+                new Game
+                {
+                    GameId = 2,
+                    GameTitle = "Fifa 19"
+                },
+
+                new Game
+                {
+                    GameId = 3,
+                    GameTitle = "LoL"
+                },
+
+                new Game
+                {
+                    GameId = 4,
+                    GameTitle = "CS"
+                },
+
+                new Game
+                {
+                    GameId = 5,
+                    GameTitle = "Angry Birds"
+                }
+            };
+
+            games.ForEach(s => context.Games.Add(s));
+            context.SaveChanges();
+
+        }
+    
+    }
+}
